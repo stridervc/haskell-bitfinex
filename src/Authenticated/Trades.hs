945d208c57a6469a407404ca9245c83d1a360d19
Fred Strauss <fred@strider.co.za>
2022-05-10 14:08:13 +0200

    Add Trades

---
+++ src/Authenticated/Trades.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE DeriveGeneric #-}
+
+module Authenticated.Trades
+  ( Trade (..)
+  , trades
+  ) where
+
+import Common
+import Data.Aeson
+import GHC.Generics
+
+newtype TradeRaw = TradeRaw [Value] deriving (Eq, Show, Generic)
+instance FromJSON TradeRaw
+
+data Trade = Trade
+  { tradeID           :: Int
+  , tradePair         :: String
+  , tradeCreate       :: Int
+  , tradeOrderID      :: Int
+  , tradeAmount       :: Float  -- ^ Positive is buy, negative is sell
+  , tradePrice        :: Float
+  , tradeOrderType    :: String
+  , tradeOrderPrice   :: Float
+  , tradeMaker        :: Bool
+  , tradeFee          :: Float
+  , tradeFeeCurrency  :: String
+  } deriving (Eq, Show)
+
+fromRaw :: TradeRaw -> Trade
+fromRaw (TradeRaw v) = Trade (d 0) (d 1) (d 2) (d 3) (d 4) (d 5) (d 6) (d 7) (d 8) (d 9) (d 10)
+  where decode' a = case fromJSON a of
+                      Success a -> a
+                      Error e   -> error e
+        d i = decode' $ v!!i
+
+trades :: BitfinexClient -> String -> IO [Trade]
+trades client symbol = map fromRaw <$> queryBitfinexAuthenticated client ("r/wallets/" ++ symbol ++ "/hist")
