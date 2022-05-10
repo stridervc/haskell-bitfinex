a0aba3c50be488aab711fbf260322aab3d570ec3
Fred Strauss <fred@strider.co.za>
2022-05-10 14:31:24 +0200

    Convert trade time

--- src/Authenticated/Trades.hs
+++ src/Authenticated/Trades.hs
@@ -8,6 +8,8 @@ module Authenticated.Trades
 import Common
 import Data.Aeson
 import GHC.Generics
+import Data.Time (UTCTime)
+import Data.Time.Clock.POSIX (posixSecondsToUTCTime)
 
 newtype TradeRaw = TradeRaw [Value] deriving (Eq, Show, Generic)
 instance FromJSON TradeRaw
@@ -15,7 +17,7 @@ instance FromJSON TradeRaw
 data Trade = Trade
   { tradeID           :: Int
   , tradePair         :: String
-  , tradeCreate       :: Int
+  , tradeCreateTime   :: UTCTime
   , tradeOrderID      :: Int
   , tradeAmount       :: Float  -- ^ Positive is buy, negative is sell
   , tradePrice        :: Float
@@ -27,11 +29,12 @@ data Trade = Trade
   } deriving (Eq, Show)
 
 fromRaw :: TradeRaw -> Trade
-fromRaw (TradeRaw v) = Trade (d 0) (d 1) (d 2) (d 3) (d 4) (d 5) (d 6) (d 7) maker (d 9) (d 10)
+fromRaw (TradeRaw v) = Trade (d 0) (d 1) ctime (d 3) (d 4) (d 5) (d 6) (d 7) maker (d 9) (d 10)
   where decode' a = case fromJSON a of
                       Success a -> a
                       Error e   -> error e
         d i       = decode' $ v!!i
+        ctime     = posixSecondsToUTCTime $ d 2 / 1000
         maker     = (d 8 :: Int) == 1
 
 trades :: BitfinexClient -> String -> IO [Trade]
